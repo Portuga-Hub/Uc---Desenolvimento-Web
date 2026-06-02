e552c55c10ef5d3a1545e7794cefb96d4e4da02a
Portuga-Hub <pedrohenriquefernandes987@gmail.com>
2026-06-02 15:18:52 +0000

    atualização do projeto

--- script.js
+++ script.js
@@ -1,12 +1,43 @@
-async function init() {
+async function buscarCEP(){
 
-  const response = await fetch('https://jsonplaceholder.typicode.com/users');
-  const users = await response.json();
+    let cep = document.getElementById("cep");
+    let resultado = document.getElementById("resultado");
+    let mensagem = document.getElementById("mensagem");
 
-  console.log(users);
-  console.log(users[0].name);
-  console.log(users[1].email);
+    resultado.innerHTML = "";
+    mensagem.innerHTML = "";
 
+    if(cep.value == ""){
+        mensagem.innerHTML =
+        "<p class='erro'>Digite um CEP!</p>";
+        return;
+    }
+
+    try{
+
+        let resposta = await fetch(
+            `https://viacep.com.br/ws/${cep.value}/json/`
+        );
+
+        let dados = await resposta.json();
+
+        if(dados.erro){
+            mensagem.innerHTML =
+            "<p class='erro'>CEP não encontrado!</p>";
+            return;
+        }
+
+        resultado.innerHTML = `
+            <div class="info"><strong>CEP:</strong> ${dados.cep}</div>
+            <div class="info"><strong>Rua:</strong> ${dados.logradouro}</div>
+            <div class="info"><strong>Bairro:</strong> ${dados.bairro}</div>
+            <div class="info"><strong>Cidade:</strong> ${dados.localidade}</div>
+            <div class="info"><strong>Estado:</strong> ${dados.uf}</div>
+        
+        `;
+
+    }catch{
+        mensagem.innerHTML =
+        "<p class='erro'>Ta errado burro!</p>";
+    }
 }
-
-init();
